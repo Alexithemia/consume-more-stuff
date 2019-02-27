637afbf462cbbb9504022f4e2da04d1c388b2110
Eunice Min <euniceem1007@gmail.com>
2019-02-27 12:18:42 -1000

    added home component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 
 import Header from '../../components/Header';
 import Navigation from '../../components/Navigation';
+import Home from '../../components/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title={ "CMS" } />
-        <Navigation />
+        <div>
+          <Header title={"CMS"} />
+          <Navigation />
+        </div>
+        <div>
+          <Home />
+        </div>
       </div>
+
+
     );
   }
 }
