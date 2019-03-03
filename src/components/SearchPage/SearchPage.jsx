c2c2bd95907d9e6d10966ccef22173502b0c1ff9
Tyler <alexithemia@gmail.com>
2019-03-02 18:08:35 -1000

    Added search page

---
+++ src/components/SearchPage/SearchPage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './SearchPage.scss';
+
+class SearchPage extends Component {
+
+  render() {
+
+    return (
+      <div className="searchContainer">
+
+
+      </div>
+    )
+  }
+}
+
+export default SearchPage;
