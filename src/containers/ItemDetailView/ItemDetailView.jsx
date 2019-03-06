00a1819b2da9d02da24611215c2ba5a3cd49c456
Eunice Min <euniceem1007@gmail.com>
2019-03-05 14:16:10 -1000

    working out different ways to make image zoom work

--- src/containers/ItemDetailView/ItemDetailView.jsx
+++ src/containers/ItemDetailView/ItemDetailView.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
-import ReactModal from 'react-modal';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import ReactImageMagnify from 'react-image-magnify';
 import './ItemDetailView.scss';
-
 import { loadPost } from '../../actions';
-import ItemSendMessage from '../ItemSendMessage';
 import ImageList from '../../components/ImageList';
 
 class ItemDetailView extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      showModal : false,
-      selectedImg: ""
-    };
-
-    this.toggleModal = this.toggleModal.bind(this);
     this.changePhoto = this.changePhoto.bind(this);
     this.deletePost = this.deletePost.bind(this);
   }
@@ -28,23 +20,25 @@ class ItemDetailView extends Component {
 
   componentDidMount() {
     this.props.loadPost(this.props.match.params.id)
+
+    // const script = document.createElement("script");
+    // console.log(script)
+
+    // script.src = "../../magnify.js";
+    // script.body = ("myimage", 3)
+    // script.async = true;
+
+    // document.body.appendChild(script);
   }
-  
+
   componentDidUpdate() {
     if (this.state.selectedImg === "" && this.props.selectedPost.image) {
       if (this.props.selectedPost.image[0]) {
         this.setState({ selectedImg: this.props.selectedPost.image[0].url })
-        
       } else {
         this.setState({ selectedImg: "https://s3-us-west-2.amazonaws.com/alexithemia-cms-imagestore/no-image.jpg" })
       }
     }
-  }
-  
-  toggleModal() {
-    return this.setState({
-      showModal : !this.state.showModal
-    });
   }
 
   changePhoto(e) {
@@ -57,7 +51,6 @@ class ItemDetailView extends Component {
 
   render() {
     const { id, category, description, dimensions, image, manufacturer, model, title, price, postCondition, postStatus, user, notes, views, created_at, updated_at, user_id } = this.props.selectedPost;
-
     return (
       <div className="itemDetailViewContainer">
         {this.props.selectedPost.user && (
@@ -66,43 +59,21 @@ class ItemDetailView extends Component {
               <div className="imgNavBar">
                 <ImageList images={image} changePhoto={this.changePhoto}></ImageList>
               </div>
-
               <div className="imgBox">
                 <img id="img" className="img" src={this.state.selectedImg} alt="" />
+                <div id="myresult" className="img-zoom-result"></div>
               </div>
-
               <div className="titleContainer">
                 <h1 className="title">{title}: {postStatus.name}</h1>
                 <div className="user">by {user.username}</div>
                 <div className="descriptionContainer">
-                  <div className="price">
-                    Price: <span className="dynamic-data">${ price }</span>
-                  </div>
-
-                  <div className="category">
-                    Category: <span className="dynamic-data">{ category.name }</span>
-                  </div>
-
-                  <div className="description">
-                    Description: <span className="dynamic-data">{ description }</span>
-                  </div>
-
-                  <div className="posted">
-                    Posted: <span className="dynamic-data">{ created_at }</span>
-                  </div>
-
-                  <div className="updated">
-                    Last updated: <span className="dynamic-data">{ updated_at }</span>
-                  </div>
-
-                  <div className="notes">
-                    Note: <span className="dynamic-data">{ notes }</span>
-                  </div>
-
-                  <div className="views">
-                    Viewed: <span className="dynamic-data">{ views } times</span>
-                  </div>
-
+                  <div className="price">Price: ${price}</div>
+                  <div className="category">Category: {category.name}</div>
+                  <div className="description">{description}</div>
+                  <div className="posted">Posted: {created_at}</div>
+                  <div className="updated">Last updated: {updated_at}</div>
+                  <div className="notes">Note: {notes}</div>
+                  <div className="views">Viewed: {views}</div>
                   {this.props.selectedPost.user_id === this.props.id || this.props.isAdmin ?
                     <div className="options">
                       <Link to={`/edit/${id}`} className="editPost">Edit</Link>
@@ -110,46 +81,26 @@ class ItemDetailView extends Component {
                     </div>
                     :
                     <div className="options">
-                      {/* <Link to={`/message/${ user }/${ id }`} className="messagePoster">Message Me</Link> */}
-                      <div className="messagePosterContainer">
-                        <button onClick={ this.toggleModal } className="messagePoster">
-                          Message Me
-                        </button>
-
-                        <ReactModal
-                          isOpen={ this.state.showModal }
-                          onRequestClose={ this.toggleModal }
-                          className="modal"
-                          overlayClassName="overlay"
-                          shouldCloseOnOverlayClick={ true }
-                          ariaHideApp={ false }
-                        >
-                          <ItemSendMessage />
-                        </ReactModal>
-                      </div>
+                      <Link to={`/message/${user_id}/${id}`} className="messagePoster">Message Me</Link>
                     </div>
                   }
                 </div>
               </div>
             </div>
             <div className="secondContainer">
-              <h2 className="descriptionTitle">Product Description</h2>
-
+              <h2 className="descriptionTitle">Product description</h2>
               <div className="productDescription">
                 <div className="condition">
-                  Condition: <span className="dynamic-data">{ postCondition.name }</span>
+                  Condition: {postCondition.name}
                 </div>
-
                 <div className="manufacturer">
-                  Manufacturer: <span className="dynamic-data">{ manufacturer }</span>
+                  Manufacturer: {manufacturer}
                 </div>
-
                 <div className="model">
-                  Model: <span className="dynamic-data">{ model }</span>
+                  Model: {model}
                 </div>
-
                 <div className="dimension">
-                  Dimensions: <span className="dynamic-data">{ dimensions }</span>
+                  Dimensions: {dimensions}
                 </div>
               </div>
             </div>
